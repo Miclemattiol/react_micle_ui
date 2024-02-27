3e48ec163fb51d9fb0396ae5849aad9e4d7f3a7c
Michael Mattiolo <miclemattiol@gmail.com>
2024-02-27 18:30:49 +0100

    Input is not a client component anymore

--- Input/index.tsx
+++ Input/index.tsx
@@ -1,64 +1,37 @@
-"use client";
-
-import { useEffect, useState } from "react";
 import styles from "./Input.module.scss";
 import classNames from "classnames";
 
-type InputProps = React.InputHTMLAttributes<HTMLInputElement>;
+type InputProps = React.ClassAttributes<HTMLInputElement> &
+	React.InputHTMLAttributes<HTMLInputElement>;
 
 const Input = ({
 	type,
 	value,
 	onChange,
+	onBlur,
+	onFocus,
 	placeholder,
 	className,
 	id,
 	name,
-	...props
+	ref,
 }: InputProps) => {
-	const [_value, _setValue] = useState(value);
-	const [selected, setSelected] = useState(false);
-
-	const [filling, setFilling] = useState(false);
-
-	useEffect(() => {
-		if (_value) {
-			console.log("Setting filling to true");
-			setFilling(true);
-		} else {
-			console.log("Setting filling to false");
-			setFilling(false);
-		}
-	}, [_value]);
-
 	return (
-		<div className={classNames(classNames, styles.Input)}>
+		<div className={classNames(className, styles.Input)}>
 			<div className={styles.inputWrapper}>
 				<input
-					onChange={(event) => {
-						_setValue(event.target.value);
-					}}
-					onFocus={() => {
-						setSelected(true);
-					}}
-					onBlur={() => {
-						setSelected(false);
-					}}
+					onChange={onChange}
+					onBlur={onBlur}
+					onFocus={onFocus}
 					type={type}
 					placeholder=" "
 					id={id}
 					name={name}
+					value={value}
+					ref={ref}
 				/>
 			</div>
-			<label
-				htmlFor={id}
-				className={classNames(
-					styles.placeholder,
-					filling || selected ? styles.filling : undefined
-				)}
-			>
-				{placeholder}
-			</label>
+			<label htmlFor={id}>{placeholder}</label>
 		</div>
 	);
 };
